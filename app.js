52840f62bc6a39192bcd66d5099bd3fabf48f2a4
Varsken Pitiakhsh <schal13@freeuni.edu.ge>
2017-07-26 18:26:15 +0400

    იო დავითრიე

--- app.js
+++ app.js
@@ -43,4 +43,14 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
+// აქედან უკვე შეგვიძლია გავაგრძლოთ აპლიკაციის კოდის შეკვრა
+var init = function (io) {
+    console.log('!@#: Initializing...');
+    var serve = require('./src/server').serve;
+    serve(io);
+};
+
+module.exports = {
+    app: app,
+    init: init
+};
---
+++ src/server.js
@@ -0,0 +1,11 @@
+/**
+ * Created by aro on 7/26/17.
+ */
+function serve(io) {
+    console.log('!@#: Starting game');
+}
+
+// არ მუშაობს პირდაპირ exports = ... რატომ არ ვიცი
+module.exports = {
+    serve: serve
+};
